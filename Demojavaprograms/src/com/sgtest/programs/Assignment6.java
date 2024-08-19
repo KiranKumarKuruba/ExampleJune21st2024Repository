1e8f37c60f708288a64aef9d55346f0c0f9d31db
KiranKumarKuruba <kirankuruba107@gmail.com>
2024-08-19 19:22:51 +0530

    Assignment6.java

---
+++ Demojavaprograms/src/com/sgtest/programs/Assignment6.java
@@ -0,0 +1,11 @@
+package com.sgtest.programs;
+
+
+public class Assignment6 {
+    public static void main(String[] args) {
+
+        String s = "Hello world";
+        System.out.println(s);
+    }
+
+}
